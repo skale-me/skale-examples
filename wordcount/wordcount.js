6ee841344e0c9c3b5fbdea63e3fa43d1e3bba281
Cédric Artigue <cedric.artigue@ugrid.net>
2016-03-31 18:53:42 +0200

    Added wordcount on james joyce ulysse

---
+++ wordcount/wordcount.js
@@ -0,0 +1,11 @@
+#!/usr/bin/env node
+
+var sc = require('skale-engine').context();
+
+sc.textFile(__dirname + '/james_joyce_ulysse.txt')
+	.flatMap(line => line.split(' '))
+	.map(word => [word, 1])
+	.reduceByKey((a, b) => a + b, 0)
+	.collect()
+	.on('data', console.log)
+	.on('end', sc.end)
