d7e5f76344afd82b373bc237a2b9213c0511e122
Philippe Bergheaud <felix@linux.vnet.ibm.com>
2016-04-18 18:20:06 +0200

    Since 0.4.0, all functions return streams. This fixes pi.js for the new API.
    Signed-off-by: Philippe Bergheaud <felix@linux.vnet.ibm.com>

--- pi/pi.js
+++ pi/pi.js
@@ -9,7 +9,6 @@ function sample() {
 	return ((x * x + y * y) < 1) ? 1 : 0;
 }
 
-sc.range(0, NUM_SAMPLES).map(sample).reduce((a, b) => a + b, 0).then(function(count) {
+sc.range(0, NUM_SAMPLES).map(sample).reduce((a, b) => a + b, 0).on('data', function(count) {
 	console.log('Pi is roughly ', (4.0 * count / NUM_SAMPLES));
-	sc.end();
-})
+}).on('end', sc.end);
